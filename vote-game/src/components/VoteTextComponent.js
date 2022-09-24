c8910a9958470e724c3585c43ad93dd5c710d7a9
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2022-09-24 09:22:08 +0300

    CM-42: Create Vote Component

---
+++ vote-game/src/components/VoteTextComponent.js
@@ -0,0 +1,45 @@
+import {getSettings} from "../settings";
+
+const settings = getSettings();
+
+export class VoteTextComponent extends BaseComponent {
+    async onLoad() {
+        const instanceId = await this.plugin.world.getInstanceID();
+        await this.updateTextValue(instanceId);
+    }
+
+    async onClick() {
+        const instanceId = await this.plugin.world.getInstanceID();
+        const resp = await fetch(`${settings.host}/vote/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                instanceId,
+                value: this.fields.name,
+                userId: await this.plugin.user.getID()
+            })
+        });
+
+        if (resp.status === 200) {
+            await this.updateTextValue(instanceId);
+        }
+    }
+
+    async updateTextValue(instanceId) {
+        const regex = new RegExp('^.*\\(\\d*\\)$');
+        // textValue need to be in 'value (total_votes)' format in order to update it.
+        if (!regex.test(this.fields.textValue)) {
+            return;
+        }
+
+        const count = await fetch(`${settings.host}/vote/${instanceId}/${this.fields.name}`)
+            .then(resp => resp.json())
+            .then(json => json.count);
+        console.log(count);
+        const text = this.fields.textValue.substring(0, this.fields.textValue.lastIndexOf('('));
+        const textValue = `${text}(${count})`;
+        await this.plugin.objects.update(this.objectID, {textValue});
+    }
+}
